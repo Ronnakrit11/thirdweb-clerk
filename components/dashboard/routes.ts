bee694bad2cc6028a9ed04fd487235d4447bb910
21 Tech Enterprise <59superrichsol@gmail.com>
2024-12-06 08:51:04 +0700

    done

--- components/dashboard/routes.ts
+++ components/dashboard/routes.ts
@@ -1,22 +1,26 @@
 export const dashboardRoutes = [
-    {
-      href: "/dashboard/clients",
-      label: "Clients",
-    },
-    {
-      href: "/dashboard/services",
-      label: "Services",
-    },
-    {
-      href: "/dashboard/quotations",
-      label: "Quotations",
-    },
-    {
-      href: "/dashboard/invoices",
-      label: "Invoices",
-    },
-    {
-      href: "/dashboard/aifeatures",
-      label: "Ai-features",
-    },
-  ] as const;
+  {
+    href: "/dashboard",
+    label: "Dashboard",
+  },
+  {
+    href: "/dashboard/clients",
+    label: "Clients",
+  },
+  {
+    href: "/dashboard/services",
+    label: "Services",
+  },
+  {
+    href: "/dashboard/quotations",
+    label: "Quotations",
+  },
+  {
+    href: "/dashboard/invoices",
+    label: "Invoices",
+  },
+  {
+    href: "/dashboard/aifeatures",
+    label: "Ai-features",
+  },
+] as const;
